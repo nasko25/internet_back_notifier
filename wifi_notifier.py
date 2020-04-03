3aa41df0e111c04c546469054c7bfae084ea5dcd
nasko25 <apashov0@gmail.com>
2020-04-03 16:25:06 +0300

    timeouts work

--- wifi_notifier.py
+++ wifi_notifier.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import subprocess
+from subprocess import TimeoutExpired
 import argparse
 import platform
+import time
 
 # set up the command line arguments
 parser = argparse.ArgumentParser(description="Notifier when wifi is back up.")
                                                                           # choose better default targer
 parser.add_argument("--host", metavar="hostname", type=str, nargs='?', default="google.com", help="The hostname to ping")
 parser.add_argument("--count", metavar="N", type=str, nargs="?", default="2", help="The number of times the program should attemp to ping the host.")
+parser.add_argument("--silent", action="store_true", help="Silent the output. Only show when network connection is back up.")
 args = parser.parse_args()
 
 # ping -c 2 google.com     for linux
@@ -21,4 +24,22 @@ else:
     parameter = "-c"
 
 print("Will start pinging", args.host, "until wifi is back up.")
-subprocess.call(["ping", parameter, args.count, args.host])
+
+count_expired = 0
+while True:
+    time.sleep(3)
+    try:
+        output = subprocess.run(["ping", parameter, args.count, "-w", "2",args.host], timeout=2, capture_output=True)
+        print(output)
+        if b"Temporary failure in name resolution" in output.stderr or output.returncode==2:
+            raise TimeoutExpired("Cannot validate hostname", timeout=2)
+        break
+    except(TimeoutExpired):
+        count_expired+=10
+    if not args.silent and count_expired % 10 == 0:
+        print("Trying to connect...")
+
+print("There is network connection")
+
+# TODO persistence
+# check for network connection every 10 minutes?
