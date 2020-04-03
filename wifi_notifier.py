36bacc07d5fc14ef2ee539e44e660b747b87e1a4
nasko25 <apashov0@gmail.com>
2020-04-03 17:34:10 +0300

    changed the formatter to show default values, moved the sleep to after executing the initial ping
    also commented out some output and modified the --help menu from argparse

--- wifi_notifier.py
+++ wifi_notifier.py
@@ -6,13 +6,13 @@ import argparse
 import platform
 import time
 
-# set up the command line arguments
-parser = argparse.ArgumentParser(description="Notifier when wifi is back up.")
+# set up the command line arguments                                             The formatter will show default values
+parser = argparse.ArgumentParser(description="Notifier when wifi is back up.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
                                                                           # choose better default targer
 parser.add_argument("--host", metavar="hostname", type=str, nargs='?', default="google.com", help="The hostname to ping")
 parser.add_argument("--count", metavar="N", type=str, nargs="?", default="2", help="The number of times the program should attemp to ping the host.")
 parser.add_argument("--silent", action="store_true", help="Silent the output. Only show when network connection is back up.")
-parser.add_argument("--seconds", type=int, choices=range(1, 10000), nargs="?", default=3, help="How many seconds between the ping requests when there is no network connection (in seconds in the range  1-10000).")
+parser.add_argument("--seconds", type=int, choices=range(1, 10000), metavar="1-10000", nargs="?", default=3, help="How many seconds between the ping requests when there is no network connection (in seconds in the range  1-10000).")
 args = parser.parse_args()
 
 # ping -c 2 google.com     for linux
@@ -28,10 +28,9 @@ print("Will start pinging", args.host, "until wifi is back up.")
 
 count_expired = 0
 while True:
-    time.sleep(args.seconds)
     try:
         output = subprocess.run(["ping", parameter, args.count, "-w", "2",args.host], timeout=2, capture_output=True)
-        print(output)
+        # print(output)
         if b"Temporary failure in name resolution" in output.stderr or output.returncode==2:
             raise TimeoutExpired("Cannot validate hostname", timeout=2)
         break
@@ -39,6 +38,7 @@ while True:
         count_expired+=10
     if not args.silent and count_expired % 10 == 0:
         print("Trying to connect...")
+    time.sleep(args.seconds)
 
 print("There is network connection")
 
